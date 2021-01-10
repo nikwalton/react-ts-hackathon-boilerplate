aeae40a01a95b7fbd18c376c191211371a120c1f
Nikolaus Walton <nikawalton2@gmail.com>
2021-01-10 01:49:54 -0800

    Started colour scheme, Fixed things with the hero section and the navbar

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -1,17 +1,27 @@
 import React from 'react';
-import {Container, Row, Col} from 'react-bootstrap';
-
+import {Jumbotron, Container, Button, Row} from 'react-bootstrap';
+import { textChangeRangeIsUnchanged } from 'typescript';
 
 function Hero() {
+    const JumboStyle = {
+        background: "#222831"
+    }
+    const TextStyle = {
+        color: "white"
+    }
+     
     return (
         <div>
-            <Container>
-                <Row>
-                    <Col>
-                    <p>This is the hero page.</p>
-                    </Col>
-                </Row>
-            </Container>
+            <Jumbotron fluid style={JumboStyle} className='shadow'>
+                <Container style={{paddingTop: "10em", paddingBottom: "10em"}}>
+                    <h1 style={TextStyle}>What The Hack? TSHack</h1>
+                    <p style={TextStyle}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    <Button variant="light">Register</Button>{' '}
+                    <Button variant="light">Volunteer</Button>{' '}
+                    <Button variant="light">Sponsor</Button>{' '}
+                </Container>
+            </Jumbotron>
+
         </div>
     );
 }
