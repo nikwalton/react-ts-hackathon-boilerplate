701421ebf00b394bc5ba6d7bd717d19a0d3d97da
Nikolaus Walton <nikawalton2@gmail.com>
2021-02-06 02:13:04 -0800

    Edit about section, added images from unsplash

--- frontend/src/components/About.tsx
+++ frontend/src/components/About.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container, Row, Col, Image } from 'react-bootstrap'
 import MediaQuery from 'react-responsive'
+import image1 from '../images/alex-kotliarskyi-ourQHRTE2IM-unsplash.jpg'
+import image2 from '../images/luca-bravo-XJXWbfSo2f0-unsplash.jpg'
 
 function About() {
 
@@ -31,18 +33,26 @@ function About() {
                     ullamcorper pretium ante. Aenean sit amet sapien quis elit efficitur condimentum. Suspendisse tortor quam,
                     vulputate nec turpis ac, posuere fringilla lectus. Fusce pretium tortor ipsum, in cursus eros vestibulum eu.
                     Etiam eu erat vel nisl sollicitudin gravida. Aliquam diam augue, eleifend in lectus sit amet, suscipit pharetra ante.
-                        Pellentesque aliquam posuere orci et porta.</p>
+                        Pellentesque aliquam posuere orci et porta. Fusce pretium tortor ipsum, in cursus eros vestibulum eu.
+                    Etiam eu erat vel nisl sollicitudin gravida. Aliquam diam augue, eleifend in lectus sit amet, suscipit pharetra ante.
+                        Pellentesque aliquam posuere orci et porta enean sit amet sapien quis elit efficitur condimentum. Suspendisse tortor quam,
+                    vulputate nec turpis ac, posuere fringilla lectus. Pellentesque aliquam posuere orci et porta. Fusce pretium tortor ipsum, in cursus eros vestibulum eu.
+                    Etiam eu erat vel nisl sollicitudin gravida. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur tortor quam, fermentum in augue vitae,
+                    ullamcorper pretium ante.</p>
+            
                 </Col>
                 <MediaQuery minWidth={768}>
                     <Col>
-                        
+                    <Image src={image1} className="shadow" style={{height:300, marginLeft:200, marginBottom: 50}} rounded thumbnail />
                     </Col>
                 </MediaQuery>
+               
             </Row>
+            
             <Row>
             <MediaQuery minWidth={768}>
                 <Col>
-                    
+                    <Image src={image2} className="shadow" style={{height:300,marginLeft:200 }} rounded thumbnail/>
                 </Col>
             </MediaQuery>    
                 <Col>
@@ -51,20 +61,10 @@ function About() {
                     enim purus interdum lectus, sit amet efficitur lacus risus non mi. Maecenas vel magna eget neque faucibus commodo ut quis dolor. 
                     Suspendisse aliquam efficitur fringilla. Maecenas ac efficitur mi. Pellentesque placerat lorem cursus magna mattis consequat. 
                     Nullam sit amet magna dui. In hac habitasse platea dictumst. Nullam rutrum, metus a accumsan vehicula, tellus nunc viverra ligula,
-                     ac laoreet sapien lectus eu eros. Sed in augue massa.</p>
+                     ac laoreet sapien lectus eu eros. Sed in augue massa. Integer sit amet condimentum tortor, quis sagittis sem. Proin id nibh congue, eleifend nisl eget, vestibulum elit. 
+                    In nec velit sapien. Donec metus velit, ultrices eu metus id, dignissim pulvinar magna. Vestibulum pulvinar, nisi a facilisis porta, 
+                    enim purus interdum lectus, sit amet efficitur lacus risus non mi.</p>
                 </Col>
-            </Row>
-            <Row>
-                <Col>
-                    <p style={ParaStyle}>Nam aliquam justo lectus, a scelerisque lectus iaculis a. Fusce pulvinar et odio et convallis.
-                     Morbi imperdiet placerat ligula. Nunc accumsan fringilla libero, sit amet gravida justo tristique eget.
-                      Nulla eget lacus dapibus, rutrum est quis, sodales ligula. Aliquam sodales elit at ullamcorper finibus.
-                       Aenean elementum cursus tellus. Aenean at vestibulum ante. Pellentesque ut sapien at nunc dictum imperdiet.</p>
-                </Col>
-                <MediaQuery minWidth={768}>
-                    <Col>
-                    </Col>
-                </MediaQuery>
             </Row>
             <br/>
             <br/>
