f9b76b326df4169eb63800b0b313f2a03c64dc97
Nikolaus Walton <nikawalton2@gmail.com>
2021-03-11 18:09:25 -0800

    Add images to the team section, final commit moving onto new projects

--- frontend/src/components/Team.tsx
+++ frontend/src/components/Team.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import {Container, Col, Row} from 'react-bootstrap'
+import {Container, Col, Row, Image} from 'react-bootstrap'
+import placeholder from '../images/avatar-placeholder.png'
 
 function Team() {
     const ContainerStyle = {
@@ -16,6 +17,15 @@ function Team() {
                 <Col>
                     <h1 className='display-4' style={TextStyle}>Team</h1>
                 </Col>
+            </Row>
+
+            <Row className=' justify-content-md-center'>
+                <Image src={placeholder} style={{ height: "170px" }} className='m-auto p-3' roundedCircle />
+                <Image src={placeholder} style={{ height: "170px" }} className='m-auto p-3' roundedCircle />
+                <Image src={placeholder} style={{ height: "170px" }} className='m-auto p-3' roundedCircle />
+                <Image src={placeholder} style={{ height: "170px" }} className='m-auto p-3' roundedCircle />
+                <Image src={placeholder} style={{ height: "170px" }} className='m-auto p-3' roundedCircle />
+                <Image src={placeholder} style={{ height: "170px" }} className='m-auto p-3' roundedCircle />
             </Row>
         </Container>
     );
